e9a69a3431ab68195d9d294de25ff549797b01a6
wave <sarin.h@infoquest.co.th>
2024-07-03 17:05:57 +0700

    update add mini project lotto

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-me = prompt("Hello World! This is my first JavaScript code in a website.")
-console.log("Hello " + me)
+lotto = prompt("Enter your Lotto Number:")
+document.getElementById("input").innerHTML = "Your Input : " + lotto;
+document.getElementById("result").innerHTML =
+  "Number is Lotto : " + Math.floor(Math.random() * 100);
