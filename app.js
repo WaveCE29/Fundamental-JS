c409b4aba0dd37545bfa05a4cd905cce2cb23989
wave <sarin.h@infoquest.co.th>
2024-07-09 09:25:45 +0700

    add lean about compare condition

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-lotto = prompt("Enter your Lotto Number:")
-document.getElementById("input").innerHTML = "Your Input : " + lotto;
-document.getElementById("result").innerHTML =
-  "Number is Lotto : " + Math.floor(Math.random() * 100);
+// lotto = prompt("Enter your Lotto Number:")
+// document.getElementById("input").innerHTML = lotto;
+// document.getElementById("result").innerHTML = "Number is Lotto : " + Math.floor(Math.random() * 100);
+
+console.log(1 == 1);
+console.log(1 == "1");
+console.log(1 === "1");
+console.log(1 == 2);
+
+console.log(1 != 2);
+console.log(1 != "1");
+console.log(1 !== "1");
+
+console.log("4" !== 4);
+
+
+console.log(10+10 <= -20);
+
+
