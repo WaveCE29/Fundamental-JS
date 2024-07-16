7faf2f86b8ba83834137edad71826ca4bcece30b
wave <sarin.h@infoquest.co.th>
2024-07-16 11:49:10 +0700

    feat : lean if condition

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-// lotto = prompt("Enter your Lotto Number:")
-// document.getElementById("input").innerHTML = lotto;
-// document.getElementById("result").innerHTML = "Number is Lotto : " + Math.floor(Math.random() * 100);
-
-console.log(1 == 1);
-console.log(1 == "1");
-console.log(1 === "1");
-console.log(1 == 2);
-
-console.log(1 != 2);
-console.log(1 != "1");
-console.log(1 !== "1");
-
-console.log("4" !== 4);
-
-
-console.log(10+10 <= -20);
-
-
+let age = prompt("Enter your age:");
+if (age > 18) {
+  document.getElementById("content").innerHTML =
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/gcJm8eNaiF0?si=bimYSoMvyP6PJ_C-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+} else {
+  document.getElementById("content").innerHTML =
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/QaG5nq2D7i8?si=ncidGNIChfTEFjks" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+}
