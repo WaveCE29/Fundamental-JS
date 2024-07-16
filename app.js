47826cd65ffd46b90f828f34485f4964cd3bdd9b
wave <sarin.h@infoquest.co.th>
2024-07-16 16:58:53 +0700

    feat : Lotto v.2

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let age = prompt("Enter your age:");
-if (age > 18) {
-  document.getElementById("content").innerHTML =
-    '<iframe width="560" height="315" src="https://www.youtube.com/embed/gcJm8eNaiF0?si=bimYSoMvyP6PJ_C-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
-} else {
-  document.getElementById("content").innerHTML =
-    '<iframe width="560" height="315" src="https://www.youtube.com/embed/QaG5nq2D7i8?si=ncidGNIChfTEFjks" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+let lotto_number = prompt("Enter your lotto number :");
+let random_number = Math.floor(Math.random() * 1000)
+document.getElementById("random").innerHTML = random_number;
+if (lotto_number == random_number) {
+  document.getElementById("result").innerHTML = "You win!";
+}else{
+  document.getElementById("result").innerHTML = "You lose!";
 }
